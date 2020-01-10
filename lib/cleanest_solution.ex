fadedc0e7e5e41b26474932067cd3bc091af696c
Adz <email@test.example>
2020-01-10 21:52:06 +0000

    Stuff

--- lib/cleanest_solution.ex
+++ lib/cleanest_solution.ex
@@ -108,6 +108,118 @@ end
 
 
 
+defprotocol Shape do
+  def area(shape)
+end
+
+
+defimpl Shape, for: Square do
+  def area(shape) do
+  end
+end
+
+
+# /sf sdakfh sdalhjdskl
+
+
+# This improves the solution a bit by allowing multiple args for the shape protocol.
+defmodule Square do
+  defstruct [:side]
+end
+
+# we have to defstruct still because protocols...
+defmodule Area do
+  defstruct []
+end
+
+defprotocol Shape do
+  def calculate(shape, calculation)
+end
+
+# defprotocol AreaProtocol do
+#   def calculate(shape)
+# end
+
+defimpl Shape, for: Square do
+  def calculate(%Square{side: side}, calculation) do
+    side * side
+  end
+end
+
+# defimpl Shape, for: Area do
+#   def calculate(%Area{}, shape) do
+#     AreaProtocol.calculate(shape)
+#   end
+# end
+
+
+# Make some shapes
+defmodule Square do
+  defstruct [:side]
+end
+
+defmodule Circle do
+  defstruct [:radius]
+end
+
+# define the protocol
+defprotocol Shape do
+  def area(shape)
+end
+
+# implement it for the Shape type
+defimpl Shape, for: Area do
+  def area(shape) do
+    shape.side * shape.side
+  end
+end
+
+# implement it for the Circle type
+defimpl Shape, for: Circle do
+  def area(shape) do
+    shape.radius * shape.radius * 3.14
+  end
+end
+
+defprotocol Shape do
+  def perimeter(shape)
+end
+
+defprotocol ShapeExtended do
+  def perimeter(shape)
+end
+
+defprotocol Perimeter do
+  def calculate(shape)
+end
+
+ShapeExtended.perimeter(shape)
+
+
+
+defmodule Square do
+  defstruct [:side]
+end
+
+defmodule Circle do
+  defstruct [:radius]
+end
+
+defprotocol Area do
+  def calculate_for(shape)
+end
+
+defprotocol Perimeter do
+  def calculate_for(shape)
+end
+
+
+Area.calculate_for(%Square{side: 10})
+Perimeter.calculate_for(%Circle{radius: 10})
+
+
+
+
 
 
 
